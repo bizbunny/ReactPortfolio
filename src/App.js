481e412c1cbe0523bc605b7e84fd75407acb6e74
Vi-Anh Nguyen <41245033+bizbunny@users.noreply.github.com>
2021-06-16 13:35:46 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Vi-Anh Nguyen</h1>
+      <h2>Art</h2>
+      <h2>Games</h2>
+      <h2>Codes</h2>
+    </div>
+  );
+}
